a3cd7e50b36bb19dc3d7283efac2c58fde700189
Hoa <danghuyhoa@gmail.com>
2020-04-06 15:09:17 +0700

    Update sample data

--- database/seeds/BooksTableSeeder.php
+++ database/seeds/BooksTableSeeder.php
@@ -15,10 +15,19 @@ class BooksTableSeeder extends Seeder
         $book->id = 1;
         $book->title = "Tri Thức Về Vạn Vật - Một Thế Giới Trực Quan Chưa Từng Thấy";
         $book->authors = "DK";
-//        $book->isbn = "8935251408089";
         $book->status_id = 1;
         $book->category_id = 1;
         $book->library_id = 1;
         $book->save();
+
+        $book2 = new \App\Book();
+        $book2->id = 2;
+        $book2->title = "Chiến lược đại dương xanh";
+        $book2->authors = "W. Chan Kim & Renee Mauborgne";
+        $book2->status_id = 1;
+        $book2->category_id = 1;
+        $book2->library_id = 1;
+        $book2->note = "Sách Agilead tặng nhân dịp sinh nhật CodeGym.";
+        $book2->save();
     }
 }
--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -13,7 +13,47 @@ class CategoriesTableSeeder extends Seeder
     {
         $category = new \App\Category();
         $category->id = 1;
-        $category->name = "Sách khoa học công nghệ";
+        $category->name = "Quản trị, doanh nghiệp";
+        $category->save();
+
+        $category = new \App\Category();
+        $category->id = 2;
+        $category->name = "Tạp chí chuyên đề, tạp chí lập trình...";
+        $category->save();
+
+        $category = new \App\Category();
+        $category->id = 3;
+        $category->name = "Lập trình";
+        $category->save();
+
+        $category = new \App\Category();
+        $category->id = 4;
+        $category->name = "Phát triển cá nhân, kỹ năng, tư duy...";
+        $category->save();
+
+        $category = new \App\Category();
+        $category->id = 5;
+        $category->name = "Giáo dục, đào tạo giảng viên...";
+        $category->save();
+
+        $category = new \App\Category();
+        $category->id = 6;
+        $category->name = "Văn học, tiểu thuyết, hồi ký...";
+        $category->save();
+
+        $category = new \App\Category();
+        $category->id = 7;
+        $category->name = "Khoa học xã hội, văn hóa nghệ thuật, tôn giáo, tâm linh, nghệ thuật...";
+        $category->save();
+
+        $category = new \App\Category();
+        $category->id = 8;
+        $category->name = "Bán hàng, Tư vấn tuyển sinh";
+        $category->save();
+
+        $category = new \App\Category();
+        $category->id = 9;
+        $category->name = "Marketing";
         $category->save();
     }
 }
